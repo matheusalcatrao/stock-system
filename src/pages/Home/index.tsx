3bbacfab1669c09af44a70ceb95ff51380c24939
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2023-02-23 23:20:24 -0300

    fix bug render many times

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,18 +6,28 @@ import { ItemComponent } from '../../components/Item'
 import { firebaseApp } from '../../firebaseConfig'
 import { collection, getFirestore, getDocs } from 'firebase/firestore'
 import { ProductType } from '../../types/Product'
+import { Spinner } from '@chakra-ui/react'
 
 export const Home: React.FC = () => {
   const [products, setProducts] = React.useState<Array<ProductType>>([])
+  const [loading, setLoading] = React.useState<boolean>(false)
 
   const loadProducts = async () => {
-    const db = getFirestore(firebaseApp)
-    const querySnapshot = await getDocs(collection(db, 'products'))
-    querySnapshot.forEach((doc) => {
-      if (!doc) return
-      const product: any = { id: doc.id, ...doc.data() }
-      setProducts([product])
-    })
+    try {
+      if (loading) return
+      setLoading(true)
+      const db = getFirestore(firebaseApp)
+      const querySnapshot = await getDocs(collection(db, 'products'))
+      querySnapshot.forEach((doc) => {
+        if (!doc) return
+        const product: any = { id: doc.id, ...doc.data() }
+        setProducts((old) => [...old, product])
+      })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   React.useEffect(() => {
@@ -27,6 +37,7 @@ export const Home: React.FC = () => {
   return (
     <Sidebar>
       <Content>
+        {loading && <Spinner />}
         {products.map((product) => (
           <ItemComponent key={product.id} product={product} />
         ))}
